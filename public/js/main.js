7108bc4c19029289f632c3c2d2435efc02384289
monotoan <bcollinsworth@gmail.com>
2015-11-22 00:15:50 -0500

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiZnVja3lvdXJhcGkiLCJhIjoiZEdYS2ZmbyJ9.6vnDgXe3K0iWoNtZ4pKvqA';
+
+var map = L.mapbox.map('app','fuckyourapi.o7ne7nmm',{  zoomControl:false }).setView([40.734801,-73.998799], 16);
+
+
+
+// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
